2953d117ff92ce27d85576d83889914be133d584
gbsxl <gasousaoli@gmail.com>
2025-03-18 14:48:55 -0300

    first-commit

---
+++ src/main/java/br/com/NinjaRegistration/NinjaRegistration/NinjaRegistrationApplication.java
@@ -0,0 +1,13 @@
+package br.com.NinjaRegistration.NinjaRegistration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NinjaRegistrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NinjaRegistrationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/NinjaRegistration/NinjaRegistration/NinjaRegistrationApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.NinjaRegistration.NinjaRegistration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NinjaRegistrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
