4d3ca5e9f846cee0ecb244fee6c35c2072ad0a94
gbsxl <gasousaoli@gmail.com>
2025-03-19 08:54:53 -0300

    create-of-controller-and-model

---
+++ src/main/java/br/com/NinjaRegistration/NinjaRegistration/NinjaController.java
@@ -0,0 +1,8 @@
+package br.com.NinjaRegistration.NinjaRegistration;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class NinjaController {
+}
